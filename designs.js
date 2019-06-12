2decc8944c9c7f97419ce152d7d7f1d00f81e157
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-06-12 21:33:50 +0530

    Update designs.js

--- designs.js
+++ designs.js
@@ -8,14 +8,16 @@ function makeGrid() {
     const gridHeight = document.getElementById("input_height").value;
     const gridWidth = document.getElementById("input_width").value;
     const pixelCanvas = document.getElementById("pixelCanvas");
-    pixelCanvas.innerText=""; // empty table
+    // pixelCanvas.innerText=""; // empty table
 
     for (let h=0; h<gridHeight; ++h) {
-        const row = pixelCanvas.insertRow(h); // insert new row at the last position
+        const row = pixelCanvas.append("<tr></tr>");
         for (let w=0; w<gridWidth; ++w) {
-            const cell = row.insertCell(w); //insert new cell at the last position
+            const cell = row.append("<td></td>");
             cell.onclick = changeColor;
         }
     }
-    event.preventDefault();
+    // event.preventDefault();
 }
+
+makeGrid()

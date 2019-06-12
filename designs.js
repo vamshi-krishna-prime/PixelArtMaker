97722c8d452be6e09828c388511df3a361391f92
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-06-12 21:35:17 +0530

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,55 +1,31 @@
-// function that builds a grid in the "container"
-function createGrid() {
-  const gridHeight = document.getElementById("input_height").value;
-  const gridWidth = document.getElementById("input_width").value;
-  const pixelCanvas = document.getElementById("pixelCanvas");
-  const container = document.getElementById("container");
-  pixelCanvas.innerText=""; // empty table
-
-  for (var rows = 0; rows < gridHeight; rows++) {
-      for (var columns = 0; columns < gridWidth; columns++) {
-          container.append("<div class='grid'></div>");
-      };
-  };
-  const grid = document.querySelector(".grid");
-  grid.width(960/gridWidth);
-  grid.height(960/gridHeight);
-};
+function changeColor() {
+    const color = document.getElementById("colorPicker").value;
+    this.style.background = color;
+}
 
 
-// function that clears the grid
-function clearGrid(){
-    $(".grid").remove();
-};
+function makeGrid() {
+    const gridHeight = document.getElementById("input_height").value;
+    const gridWidth = document.getElementById("input_width").value;
+    const pixelCanvas = document.getElementById("pixelCanvas");
+    pixelCanvas.innerText=""; // empty table
 
-// function that prompts the user to select the number of boxes in a new grid
-// the function then also creates that new grid
-function refreshGrid(){
-    // var z = prompt("How many boxes per side?");
-    clearGrid();
-    createGrid();
-};
+    for (let i = 0; i < gridHeight; i++) {
+      let row = $("<tr></tr>").appendTo(pixelCanvas);
+      for (j = 0; j < gridWidth; j++) {
+        let cell = $("<td></td>");
+        row.append(cell);
+        cell.onclick = changeColor;
 
-// create a 16x16 grid when the page loads
-// creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
-// allows the click of a button to prompt the user to create a new grid
 
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('The heading was clicked!')
-//returns
-// $(document).ready(function() {
-    createGrid(16);
 
-    grid.mouseover(function() {
-        $(this).css("background-color", "black");
-        });
-
-    const submitButton = document.querySelector('input[type=submit]');
-    submitButton.addEventListener('click', function () {
-      console.log('The heading was clicked!');
-      refreshGrid();
-      $(".grid").mouseover(function() {
-        $(this).css("background-color", "black");
-        });
-    });
-});
+// This also works :
+    // for (let h=0; h<gridHeight; ++h) {
+    //     const row = pixelCanvas.insertRow(h); // insert new row at the last position
+    //     for (let w=0; w<gridWidth; ++w) {
+    //         const cell = row.insertCell(w); //insert new cell at the last position
+    //         cell.onclick = changeColor;
+        }
+    }
+    event.preventDefault();
+}
